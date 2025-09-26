4840a95461f6edee4c28fe1b86cfc7afc3b6a38d
LS2304 <lakshyasinghal2320@gmail.com>
2025-09-27 00:35:23 +0530

    feat: setup the frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <div className="font-bold text-4xl bg-amber-100">
+      Upcoming EdTech Platform Launch!!
+    </div>
+  )
+}
+
+export default App
